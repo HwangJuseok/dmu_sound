b84bf97806e058909500b395312b764667d1f459
Frost40I <csans2644@gmail.com>
2025-05-18 21:03:12 +0900

    검색페이지 제작

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -6,7 +6,7 @@ function MainPage() {
   return (
     <main className="main-page">
       <div>
-      <SearchBar />
+      
       <Section title="다시 듣기" />
       <Section title="최신 음악" />
       <Section title="맞춤 추천 음악" />
